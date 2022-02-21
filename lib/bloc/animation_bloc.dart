6d9c078be997d27c1f85cef12dd94f730eeb5000
Sirojiddin <ffiishsh@gmail.com>
2022-02-22 01:16:20 +0500

    image +++

---
+++ lib/bloc/animation_bloc.dart
@@ -0,0 +1,11 @@
+// ignore: constant_identifier_names
+
+import 'package:equatable/equatable.dart';
+
+
+part 'position_event.dart';
+part 'position_state.dart';
+
+// class AnimationBloc extends Bloc<PositionEvent, double> {
+//   AnimationBloc()
+// }
