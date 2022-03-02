051ab0e9b946b0f9a92cc924d1de357a7b53c64e
Sirojiddin <ffiishsh@gmail.com>
2022-03-03 01:16:30 +0500

    rounte +++

---
+++ lib/route/route_generator.dart
@@ -0,0 +1,76 @@
+// ignore_for_file: unused_local_variable
+
+import 'package:flutter/material.dart';
+import 'package:math_crud/pages/active_page.dart';
+import 'package:math_crud/pages/edit_code_page.dart';
+import 'package:math_crud/pages/home_page.dart';
+import 'package:math_crud/pages/main_page.dart';
+import 'package:math_crud/pages/qr_scan_page.dart';
+import 'package:math_crud/pages/security_page.dart';
+
+class RouteGenerator {
+  static const String home = '/home';
+  static const String security = '/security';
+  static const String main = '/main';
+  static const String active = '/active';
+  static const String editCode = '/editCode';
+  static const String qrScan = '/qrScan';
+
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    final args = settings.arguments;
+    switch (settings.name) {
+      case RouteGenerator.home:
+        return MaterialPageRoute(builder: (_) => const HomePage());
+      case RouteGenerator.security:
+        return MaterialPageRoute(builder: (_) => const SecurityPage());
+      case RouteGenerator.main:
+        return MaterialPageRoute(builder: (_) => const MainPage());
+      case RouteGenerator.active:
+        return MaterialPageRoute(builder: (_) => const ActivePage());
+      case RouteGenerator.qrScan:
+        return MaterialPageRoute(builder: (_) => const QRScanPage());
+      case RouteGenerator.editCode:
+        if (args != null) {
+          return MaterialPageRoute(
+              builder: (_) => EditCodePage(
+                    data: args,
+                  ));
+        }
+        return _errorRoute();
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+          appBar: AppBar(
+            title: const Text('Error data'),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.error_outline,
+                  size: 100,
+                  color: Colors.red.shade400,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Error!',
+                  style: TextStyle(
+                    fontSize: 50,
+                    color: Colors.red.shade400,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ));
+    });
+  }
+}
