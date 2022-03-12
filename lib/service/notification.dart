a8d651799b2f86c1817adfb4f00bede81515f228
Sirojiddin <ffiishsh@gmail.com>
2022-03-12 17:52:12 +0500

    1

--- lib/service/notification.dart
+++ lib/service/notification.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 Future<void> onBackgroundMessage(RemoteMessage message) async {
