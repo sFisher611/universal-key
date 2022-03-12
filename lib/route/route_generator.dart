bcb77646c199d1df44d3628493e7237b3ad939dd
Sirojiddin <ffiishsh@gmail.com>
2022-03-12 17:51:32 +0500

    new pages +++

--- lib/route/route_generator.dart
+++ lib/route/route_generator.dart
@@ -3,10 +3,15 @@
 import 'package:flutter/material.dart';
 import 'package:math_crud/pages/active_page.dart';
 import 'package:math_crud/pages/edit_code_page.dart';
+import 'package:math_crud/pages/error_api_info_page.dart';
+import 'package:math_crud/pages/error_api_page.dart';
+import 'package:math_crud/pages/error_app_page.dart';
 import 'package:math_crud/pages/home_page.dart';
 import 'package:math_crud/pages/main_page.dart';
 import 'package:math_crud/pages/qr_scan_page.dart';
 import 'package:math_crud/pages/security_page.dart';
+
+import '../pages/error_app_info_page.dart';
 
 class RouteGenerator {
   static const String home = '/home';
@@ -15,6 +20,10 @@ class RouteGenerator {
   static const String active = '/active';
   static const String editCode = '/editCode';
   static const String qrScan = '/qrScan';
+  static const String errorApp = '/errorApp';
+  static const String errorAppInfo = '/errorAppInfo';
+  static const String errorAPI = '/errorApi';
+  static const String errorApiInfo = '/errorApiInfo';
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     final args = settings.arguments;
@@ -29,6 +38,26 @@ class RouteGenerator {
         return MaterialPageRoute(builder: (_) => const ActivePage());
       case RouteGenerator.qrScan:
         return MaterialPageRoute(builder: (_) => const QRScanPage());
+      case RouteGenerator.errorApp:
+        return MaterialPageRoute(builder: (_) => const ErrorAppPage());
+      case RouteGenerator.errorAPI:
+        return MaterialPageRoute(builder: (_) => const ErrorApiPage());
+      case RouteGenerator.errorApiInfo:
+        if (args != null) {
+          return MaterialPageRoute(
+              builder: (_) => ErrorApiInfoPage(
+                    data: args,
+                  ));
+        }
+        return _errorRoute();
+      case RouteGenerator.errorAppInfo:
+        if (args != null) {
+          return MaterialPageRoute(
+              builder: (_) => ErrorAppInfoPage(
+                    data: args,
+                  ));
+        }
+        return _errorRoute();
       case RouteGenerator.editCode:
         if (args != null) {
           return MaterialPageRoute(
